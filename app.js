1c9c8694c8836d9b23a076fa2d152d5db108f1b0
Richard <richokoye1@gmail.com>
2019-07-15 21:35:56 +0100

    add configuration

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const Joi = require('joi');
@@ -9,6 +10,11 @@ app.use(express.urlencoded( { extended: true } ));
 app.use(express.static('public'));
 app.use(helmet());
 
+
+// Configuration
+console.log('Application Name: ' + config.get('name'));
+console.log('Mail Server: ' + config.get('mail.host'));
+// console.log('Mail Password: ' + config.get('mail.password'));
 
 if(app.get('env') === 'development') {
     app.use(morgan('tiny'));
